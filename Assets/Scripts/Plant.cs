ef38cd7b1981f9a58609317dd9fb3e4dabee29b7
Syrus Bomberger <syrusbomberger@yahoo.com>
2021-02-22 15:57:22 -0600

    Tree stuff

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -6,14 +6,31 @@ public class Plant : MonoBehaviour
 {
     public PlantType Type;
     public float GrowthStage;
+    public float MaxGrowthStage;
     public bool IsGrown;
+    public bool IsBeingWatered;
 
 
     public void Start()
     {
         GrowthStage = 0;
+        MaxGrowthStage = 15;
         IsGrown = false;
     }
+
+    public void Update()
+    {
+        if (IsBeingWatered && !IsGrown)
+        {
+            GrowthStage += .00001f;
+            if(GrowthStage >= 1)
+            {
+                IsGrown = true;
+            }
+            transform.localScale += new Vector3(.01f, .01f, .01f);
+        }  
+    }
+
 
     public enum PlantType
     {
